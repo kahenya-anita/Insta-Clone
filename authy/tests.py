06a6cd6faacf08ad6cb2f19a5e82dfd08410f36e
kahenya-anita <anitakahenya1@gmail.com>
2020-10-19 00:32:47 +0300

    Tested authentication model successfully

--- authy/tests.py
+++ authy/tests.py
@@ -1,2 +1,14 @@
 from django.test import TestCase
+from .models import Profile
+class Profile(TestCase):
+    def test_fields(self):
+        item = item()
+        item.user = 'AlfredKe'
+        item.first_name = "ALfred"
+        item.last_name = "Warui"
+        item.location = "Nairobi"
+        item.profile_info = "A striving success"
+        item.save()
 
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
