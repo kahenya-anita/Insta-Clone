385435d819b5f256ab47dd0e7386e338755f434c
kahenya-anita <anitakahenya1@gmail.com>
2020-10-19 00:41:32 +0300

    Tested comment model successfully

--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import  Comment
+class Comment(TestCase):
+    def test_fields(self):
+        item = item()
+        item.user = 'Alfred'
+        item.post = "Football"
+        item.date = "10/19/moday"
+        item.body = "We're the football team your mother warned you about."
+        item.save()
 
-# Create your tests here.
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
