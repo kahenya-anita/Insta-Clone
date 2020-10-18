3542a7bd6872daa8543a8424f53a0ed83b29c389
kahenya-anita <anitakahenya1@gmail.com>
2020-10-19 01:09:14 +0300

    Tested post  models successfully

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import Post, Follow, Stream, Likes
+class Post(TestCase):
+    def test_fields(self):
+        item = item()
+        item.caption = 'Yummy'
+        item.posted = "10/19/moday"
+        item.tags = "Foodie"
+        item.user = "Alfred"
+        item.likes= "20"
+        item.save()
 
-# Create your tests here.
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
+
+class Follow(TestCase):
+    def test_fields(self):
+        item = item()
+        item.follower = 'Tatiana, Rose, Clavis'
+        item.following = "Claudia, Mark"
+        
+        item.save()
+
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
+
+class Stream(TestCase):
+    def test_fields(self):
+        item = item()
+        item.following = "Claudia, Mark"
+        item.user = "Alfred"
+        item.post = "Image of a Burger"
+        item.date = "10/19/moday"
+        item.save()
+
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
+
+class Likes(TestCase):
+    def test_fields(self):
+        item = item()
+        item.user = "Rose"
+        item.post = "Image of a Burger"
+        item.save()
+
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
+
+
