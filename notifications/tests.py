55cb5aefd4e41ff42f74861486d0f92f3c87ccce
kahenya-anita <anitakahenya1@gmail.com>
2020-10-19 00:51:53 +0300

    Tested notification model successfully

--- notifications/tests.py
+++ notifications/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Notification
 
-# Create your tests here.
+class Notification(TestCase):
+    def test_fields(self):
+        item = item()
+        item.user = 'Alfred'
+        item.post = "Post"
+        item.sender = "Crush"
+        item.notification_type = "Comment."
+        item.preview = "You look prettier than a picture."
+        item.date = "10/19/moday"
+        item.is_seen = "False"
+        item.save()
+
+        record = item.objects.get(pk1)
+        self.assertEqual(record, item)
